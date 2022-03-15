fe4b2f9a288c9120a30f5e91fda62a419aad035f
Wahala <khaelterman1981@gmail.com>
2022-03-15 10:28:25 +0100

    commit before revert

---
+++ overview.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Goodcard - track your collection of Pokémon cards</title>
+</head>
+
+<body>
+
+    <h1>Goodcard - track your collection of Pokémon cards</h1>
+
+    <ul>
+        <?php foreach ($cards as $card) : ?>
+            <li><?= $card['id'] ?></li>
+            <li><?= $card['name'] ?></li>
+            <li><?= $card['HP'] ?></li>
+            <li><?= $card['foil'] ?></li>
+            <li><?= $card['price'] ?></li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+
+</html>
